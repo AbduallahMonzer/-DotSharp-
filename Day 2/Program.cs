0f2c14128a3875aec6c93396a677e69a5b9dab7b
monzzzz <abdallahmonzer2000@gmail.com>
2025-08-14 19:11:04 +0300

    starting  the value and ref type

--- Day 2/Program.cs
+++ Day 2/Program.cs
@@ -1,4 +1,8 @@
-// Method is a block of code runs when it's only called
+// Method is a block of code performs specified task and runs when it's only called
+// Access modifiers: public - private - protected - internal 
+// Modifier : static - abstract - virtual - override - sealed - async
+// return type : void - (data type) - Task<T>
+
 using System;
 class MethodsDemo
 {
@@ -9,6 +13,11 @@ class MethodsDemo
 		MyParam(24);
 		int z = Returnable(5, 3);
 		Console.WriteLine(z);
+
+		int number = 5;
+		var demo = new MethodsDemo();
+		demo.ChangeValue(number);
+		Console.WriteLine($"This is outside the method {number}");
 	}
 	static void MyMethod()
 	{
@@ -19,10 +28,19 @@ class MethodsDemo
 	{
 		Console.WriteLine(fname + " is " + age);
 	}
-	
+
 	static int Returnable(int x, int y)
 	{
 		return x + y;
 	}
 
+
+	public void ChangeValue(int x)
+	{
+		x = 99;
+		Console.WriteLine($"This is inside the method {x}");
+	}
+
+	// pass by vale : is the default method in C# -- Reassigning the parameter doesn’t change the caller’s variable.
+	// pass by reference : gets an alias to the original -- Assigning to the parameter does change the caller’s variable.
 }
