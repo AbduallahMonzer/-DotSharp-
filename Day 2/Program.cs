9927962b08f42566c26f0724a173714dc438883a
monzzzz <abdallahmonzer2000@gmail.com>
2025-08-19 12:56:51 +0300

    Classes and objects

--- Day 2/Program.cs
+++ Day 2/Program.cs
@@ -4,10 +4,29 @@
 // return type : void - (data type) - Task<T>
 
 using System;
+using System.Globalization;
+
 class MethodsDemo
 {
+
 	static void Main(string[] args)
 	{
+		Person personOne = new Person();
+		personOne.FirstName = "Abduallah";
+		personOne.LastName = "Monzer";
+        personOne.BirthDate = DateOnly.ParseExact("22-9-2000", "d-M-yyyy", CultureInfo.InvariantCulture);
+		personOne.Country = "Egypt";
+
+		Person personTwo = new Person();
+		personTwo.FirstName = "Mohammed";
+		personTwo.LastName = "Monzer";
+		personTwo.BirthDate = DateOnly.ParseExact("22-9-2000", "d-m-yyyy", CultureInfo.InvariantCulture);
+		personTwo.Country = "Egypt";
+
+		Console.WriteLine(personOne.FirstName);
+		Console.WriteLine(personTwo.FirstName);
+
+		
 		MyMethod();
 		MyParam(25, "monzzz");
 		MyParam(24);
@@ -15,9 +34,16 @@ class MethodsDemo
 		Console.WriteLine(z);
 
 		int number = 5;
+		calculator calc = new calculator();
+
 		var demo = new MethodsDemo();
 		demo.ChangeValue(number);
 		Console.WriteLine($"This is outside the method {number}");
+		Console.WriteLine(calc.Add(2, 3));
+		Console.WriteLine(calc.Add(2, 3, 4));
+		optionalParam(7, "Abudallah", 800);
+		Console.ReadLine();
+
 	}
 	static void MyMethod()
 	{
@@ -43,4 +69,54 @@ class MethodsDemo
 
 	// pass by vale : is the default method in C# -- Reassigning the parameter doesn’t change the caller’s variable.
 	// pass by reference : gets an alias to the original -- Assigning to the parameter does change the caller’s variable.
+
+	//----------------------------------------------
+	//----------Overloaded method-------------------
+
+	class calculator
+	{
+		public int Add(int a, int b)
+		{
+			return a + b;
+		}
+
+		public int Add(int a, int b, int c)
+		{
+			return a + b + c;
+		} //Here will happen the overload -> the program will search for which method has 3 params
+	}
+
+	static void optionalParam(int no, string name, int goal, string country = "", string phone = "")
+	{
+		Console.WriteLine(no);
+		Console.WriteLine(name);
+		Console.WriteLine(goal);
+		Console.WriteLine(country);
+		Console.WriteLine(phone);
+	}
+
+	class Person
+	{
+		string ? firstName;
+		string ? lastName;
+		DateOnly birthdate;
+		string ? country;
+
+		//properties
+		public string FirstName
+		{
+			get { return firstName; }
+			set { firstName = value; }
+		}
+		public string LastName
+		{
+			get { return lastName; }
+			set { lastName = value; }
+		}
+		public DateOnly BirthDate { get; set; }
+		public string ? Country { get; set; }
+	}
+
+
 }
+
--- Day 2/obj/Debug/net9.0/Day 2.AssemblyInfo.cs
+++ Day 2/obj/Debug/net9.0/Day 2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1536f668532ea1010e005d058515ff0153c1072")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f2c14128a3875aec6c93396a677e69a5b9dab7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day 2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
