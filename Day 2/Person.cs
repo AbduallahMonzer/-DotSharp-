3f070846a17df861e7102c614c4eb72bf9e36d94
monzzzz <abdallahmonzer2000@gmail.com>
2025-09-22 11:43:50 +0300

    simplfy class by removing unused private fields

--- Day 2/Person.cs
+++ Day 2/Person.cs
@@ -2,25 +2,25 @@ using System;
 using System.Dynamic;
 public class Person
 {
-	string? firstName;
-	string? lastName;
-	DateOnly birthdate;
-	string? country;
-
-	//properties
 	public string ? FirstName{ get; set; }
+	public string? LastName { get; set; }
+	public DateOnly BirthDate { get; set; }
+	public string? Country { get; set; }
+}
 
+// string? firstName;
+	// string? lastName;
+	// DateOnly birthdate;
+	// string? country;
+	//properties
 	// public string? FirstName
 	// {
 	// 	get { return firstName; }
 	// 	set { firstName = value; }
 	// }
-	public string? LastName { get; set; }
+
 	// public string? LastName
 	// {
 	// 	get { return lastName; }
 	// 	set { lastName = value; }
 	// }
-	public DateOnly BirthDate { get; set; }
-	public string? Country { get; set; }
-}
